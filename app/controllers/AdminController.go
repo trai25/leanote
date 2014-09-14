1a3a343dab441ed1ff13c414cdb0b6749940d8a7
iiuazz <iiuazz@163.com>
2014-09-14 11:54:21 +0800

    admin page init

---
+++ app/controllers/AdminController.go
@@ -0,0 +1,21 @@
+package controllers
+
+import (
+	"github.com/revel/revel"
+)
+
+// admin 首页
+
+type Admin struct {
+	BaseController
+}
+
+// admin 主页
+func (c Admin) Index() revel.Result {
+	c.SetUserInfo()
+	c.RenderArgs["title"] = "leanote"
+	c.RenderArgs["openRegister"] = openRegister
+	c.SetLocale()
+	
+	return c.RenderTemplate("admin/index.html");
+}
